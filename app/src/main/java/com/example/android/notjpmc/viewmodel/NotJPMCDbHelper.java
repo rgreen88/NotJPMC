74d0479a9fb0583a5e17d2a069ecc05b6cbe05b6
rgreen88 <rynelgreen@gmail.com>
2018-01-10 15:57:30 -0500

    created sql entries using contract class, identified primary key by integer

--- app/src/main/java/com/example/android/notjpmc/viewmodel/NotJPMCDbHelper.java
+++ app/src/main/java/com/example/android/notjpmc/viewmodel/NotJPMCDbHelper.java
@@ -1,5 +1,7 @@
 package com.example.android.notjpmc.viewmodel;
 
+import com.example.android.notjpmc.model.NotJPMCContract;
+
 /**
  * Created by Ryne on 1/10/2018.
  */
@@ -13,6 +15,14 @@ public class NotJPMCDbHelper {
     public static final int DB_VERSION = 1;
     public static final String DB_NAME = "NotJPMC.db";
 
-
+    //sql entries and data type used
+    public static final String TEXT_TYPE = "TEXT";
+    public static final String SQL_CREATE_ENTRIES =
+            "CREATE TABLE " + NotJPMCContract.NotJPMCEntry.TABLE_NAME + " (" +
+                    NotJPMCContract.NotJPMCEntry._ID + " INTEGER PRIMARY KEY," + //id primary key
+                    NotJPMCContract.NotJPMCEntry.COLUMN_NAME_ENTRY + TEXT_TYPE + "," +
+                    NotJPMCContract.NotJPMCEntry.COLUMN_NAME_TITLE + TEXT_TYPE + "," +
+                    NotJPMCContract.NotJPMCEntry.COLUMN_NAME_SUBTITLE + TEXT_TYPE +
+                    " )"; //Took class variables to populate database in Helper from Contract
 
 }
