cb28d39000de7b6d32e408c497a9d323f3871e67
rgreen88 <rynelgreen@gmail.com>
2018-01-10 16:04:09 -0500

    extended SQLiteOpenHelper and implemented required methods

--- app/src/main/java/com/example/android/notjpmc/viewmodel/NotJPMCDbHelper.java
+++ app/src/main/java/com/example/android/notjpmc/viewmodel/NotJPMCDbHelper.java
@@ -1,12 +1,17 @@
 package com.example.android.notjpmc.viewmodel;
 
+import android.content.Context;
+
 import com.example.android.notjpmc.model.NotJPMCContract;
+
+import net.sqlcipher.database.SQLiteDatabase;
+import net.sqlcipher.database.SQLiteOpenHelper;
 
 /**
  * Created by Ryne on 1/10/2018.
  */
 
-public class NotJPMCDbHelper {
+public class NotJPMCDbHelper extends SQLiteOpenHelper{
 
     //TODO: Solve mem leak caused by static instance variable
     private static NotJPMCDbHelper notJPMCDbHelper;
@@ -16,7 +21,7 @@ public class NotJPMCDbHelper {
     public static final String DB_NAME = "NotJPMC.db";
 
     //sql entries and data type used
-    public static final String TEXT_TYPE = "TEXT";
+    private static final String TEXT_TYPE = "TEXT";
     public static final String SQL_CREATE_ENTRIES =
             "CREATE TABLE " + NotJPMCContract.NotJPMCEntry.TABLE_NAME + " (" +
                     NotJPMCContract.NotJPMCEntry._ID + " INTEGER PRIMARY KEY," + //id primary key
@@ -25,4 +30,18 @@ public class NotJPMCDbHelper {
                     NotJPMCContract.NotJPMCEntry.COLUMN_NAME_SUBTITLE + TEXT_TYPE +
                     " )"; //Took class variables to populate database in Helper from Contract
 
+    //implemented after extending SQLiteOpenHelper
+    public NotJPMCDbHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
 }
